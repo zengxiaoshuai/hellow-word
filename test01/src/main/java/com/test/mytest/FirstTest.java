786d44c149e7ff2bdd846e87a55f20758de8acfa
master <282606245@qq.com>
2021-02-02 17:05:10 +0800

    再加一句话

--- test01/src/main/java/com/test/mytest/FirstTest.java
+++ test01/src/main/java/com/test/mytest/FirstTest.java
@@ -3,5 +3,6 @@ package com.test.mytest;
 public class FirstTest {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("shenm");
     }
 }
